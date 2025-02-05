0e1e249dacbc4bcb6106be59c845121fcf312452
CyrusCorrell0 <cyruscorrell07@gmail.com>
2025-02-05 05:55:46 +0000

    hopefully fixed?

--- VideoStimulator.py
+++ VideoStimulator.py
@@ -1,10 +1,10 @@
 from moviepy import *
 import streamlit as st
-
+import time
 def combine_videos(input_file):
     top_clip = VideoFileClip(input_file)
     bottom_clip = VideoFileClip("Subway.mp4")
-    final_clip = 1;
+    final_clip = "output.mp4";
     new_height = top_clip.size[1] // 2
     scale_factor = new_height / float(top_clip.size[1])
     top_clip_resized = top_clip.resized(scale_factor)
@@ -29,9 +29,9 @@ def combine_videos(input_file):
 
 def main():
     st.title("Subway Surfer It!")
-    st.subheader("Bored?")
+    st.subheader("Lecture videos too boring? Need a way to make videos more engaging? SubwaySurferIt!\n\nThis app serves as a byproduct of ongoing work building agentic AI for short form video automation.")
     uploaded_file = st.file_uploader("Upload your video")
-
+    st.text("Made with love by Cyrus Correll")
     if not uploaded_file:
         st.button("Upload Media To Begin", disabled=True)
     else:
@@ -55,6 +55,10 @@ def main():
             with open("temp_input.mp4", "wb") as f:
                 f.write(uploaded_file.read())
             combine_videos("temp_input.mp4")
+            progress_bar=st.progress(0)
+            for i in range(101):
+                time.sleep(0.01)
+                progress_bar.progress(i)
             st.video("output.mp4")
             with open("output.mp4", "rb") as vid_file:
                 st.download_button("Download Video", vid_file, file_name="output.mp4", mime="video/mp4")
